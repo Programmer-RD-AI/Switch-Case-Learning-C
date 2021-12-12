6840a526dc1c1b615d3e6bc14fd80e7732b80cf5
Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>
2021-12-12 12:53:20 +0530

    Add files via upload

---
+++ case.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int mark;
+    char grade;
+    printf("Enter");
+    scanf("%d",&mark);
+    switch (mark) {
+        case 10 : printf('1');
+    };
+}
